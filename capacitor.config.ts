0b9333b93204bee33f154562d9008e97325c370d
SakshamBorkar <sakshamborkar23@gmail.com>
2026-07-09 14:01:12 +0530

    feat: integrate Capacitor for mobile deployment and configure static export settings

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tracksoft.bizfinder',
+  appName: 'bizfinder',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {},
+  output: 'export',
+  trailingSlash: true,
 };
 
 export default nextConfig;
